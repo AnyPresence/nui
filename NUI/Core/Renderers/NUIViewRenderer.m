8ef31668c95582629f64eeda78873b38d8eb028e
Ruslan Sokolov <mail@ruslansokolov.com>
2013-05-07 22:38:09 -0700

    Added gradient and bg image support (with new NSS properties) to UIView

--- NUI/Core/Renderers/NUIViewRenderer.m
+++ NUI/Core/Renderers/NUIViewRenderer.m
@@ -6,16 +6,72 @@
 //  Copyright (c) 2012 Tom Benner. All rights reserved.
 //
 
+#import <QuartzCore/QuartzCore.h>
+#import "NUIGraphics.h"
 #import "NUIViewRenderer.h"
+#import "UIView+NUI.h"
+
+@interface NUIViewRenderer ()
+
++ (void)removeSublayer:(CALayer *)layer withName:(NSString *)name;
+
+@end
 
 @implementation NUIViewRenderer
 
 + (void)render:(UIView*)view withClass:(NSString*)className
 {
-    if ([NUISettings hasProperty:@"background-image" withClass:className]) {
-        [view setBackgroundColor: [NUISettings getColorFromImage:@"background-image" withClass: className]];
-    } else if ([NUISettings hasProperty:@"background-color" withClass:className]) {
+    if ([NUISettings hasProperty:@"background-color" withClass:className]) {
         [view setBackgroundColor: [NUISettings getColor:@"background-color" withClass: className]];
+    }
+    
+    if ([NUISettings hasProperty:@"background-image" withClass:className]) {
+        static NSString * name = @"AP:NUI:bgImageLayer";
+        
+        CALayer * layer = [CALayer new];
+        layer.frame = view.bounds;
+        layer.name = name;
+        
+        if ([NUISettings hasProperty:@"background-image-tile" withClass:className] &&
+            [NUISettings getBoolean:@"background-image-tile" withClass:className]) {
+            layer.geometryFlipped = YES;
+            layer.transform = CATransform3DMakeScale(1.f, -1.f, 1.f);
+            layer.backgroundColor = [NUISettings getColorFromImage:@"background-image" withClass:className].CGColor;
+        } else {
+            UIImage * image = [NUISettings getImage:@"background-image" withClass:className];
+            layer.contents = (__bridge id)image.CGImage;
+            
+            if (![NUISettings hasProperty:@"background-image-stretch" withClass:className] ||
+                ![NUISettings getBoolean:@"background-image-stretch" withClass:className]) {
+                CGPoint offset = CGPointZero;
+                if ([NUISettings hasProperty:@"background-image-offset-x" withClass:className])
+                    offset.x = [NUISettings getFloat:@"background-image-offset-x" withClass:className] * CGRectGetWidth(view.bounds);
+                if ([NUISettings hasProperty:@"background-image-offset-y" withClass:className])
+                    offset.y = [NUISettings getFloat:@"background-image-offset-y" withClass:className] * CGRectGetHeight(view.bounds);;
+                
+                layer.frame = (CGRect){offset, image.size};
+            }
+        }
+        
+        if (view.nuiIsApplied) {
+            [self removeSublayer:view.layer withName:name];
+        }
+        [view.layer insertSublayer:layer atIndex:0];
+    }
+    
+    if ([NUISettings hasProperty:@"background-color-top" withClass:className]) {
+        static NSString * name = @"AP:NUI:bgGradientLayer";
+        
+        CAGradientLayer *gradientLayer = [NUIGraphics
+                                          gradientLayerWithTop:[NUISettings getColor:@"background-color-top" withClass:className]
+                                          bottom:[NUISettings getColor:@"background-color-bottom" withClass:className]
+                                          frame:view.bounds];
+        gradientLayer.name = name;
+        
+        if (view.nuiIsApplied) {
+            [self removeSublayer:view.layer withName:name];
+        }
+        [view.layer insertSublayer:gradientLayer atIndex:0];
     }
 
     [self renderSize:view withClass:className];
@@ -75,6 +131,21 @@
 
     if (height != view.frame.size.height || width != view.frame.size.width) {
         view.frame = CGRectMake(view.frame.origin.x, view.frame.origin.y, width, height);
+    }
+}
+
++ (void)removeSublayer:(CALayer *)layer withName:(NSString *)name {
+    NSInteger idx = [layer.sublayers indexOfObjectPassingTest:^BOOL(id obj, NSUInteger idx, BOOL *stop) {
+        if ([[obj name] isEqualToString:name]) {
+            *stop = YES;
+            return YES;
+        } else {
+            return NO;
+        }
+    }];
+    
+    if (idx != NSNotFound) {
+        [[layer.sublayers objectAtIndex:idx] removeFromSuperlayer];
     }
 }
 
